7d9ca888c7980d79d990e0a450ff717ce579bab8
Sanaz Bahmani <sanazbahmani35@gmail.com>
2026-01-01 22:30:30 +0200

    Implement 60s timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [secondsLeft, setSecondsLeft] = useState(60);
+  const intervalRef = useRef(null);
+
+  const startCountDown = () => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      setSecondsLeft((prev) => {
+        if (prev === 0) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={startCountDown}>Start Typing Test</button>
+      <p>{secondsLeft}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
